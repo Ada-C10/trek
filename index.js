88e6fb377459a4d87b8ba8711d1fdb8fc14ea48d
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-11-20 20:38:41 -0800

    Began working on reservations

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const loadTrips = () => {
   const tripList = $("#trip-list");
   tripList.empty();
 
-  // Actually load the pets - axis.get() returns a promise. .then is called is called on return value of axis.get().
+  // Actually load the trips - axis.get() returns a promise. .then is called is called on return value of axis.get().
   axios
     .get(URL)
     .then(response => {
@@ -42,10 +42,45 @@ const loadTrips = () => {
       reportStatus(`Encountered an error while loading pets: ${error.message}`);
     });
 };
-
+const form =
+  `<div class="container">` +
+  `<form id=trip-form>` +
+  `<label for="name">` +
+  `<input type="text" name="name">` +
+  "Name" +
+  `</input>` +
+  `</label>` +
+  `<input type="submit" name="add-reservation" value="Complete Reservation">` +
+  `</form>` +
+  `</div>`;
+const showReservationForm = trip => {
+  // Appends the form
+  $("#reservationForm").append(form);
+  // $("#reservationForm").append("<label for="name">Name</label>");
+  // $("#reservationForm").append("</div>");
+};
+const createReservation = trip => {
+  // trip.preventDefault()?
+  // Says what to do with form submit
+  // Name, email, trip name (pulled in from what they clicked on?),
+  // Requires name and email, can also accept age
+  let URL = `https://trektravel.herokuapp.com/trips/${trip.id}/reservations`;
+  axios
+    .post(URL)
+    .then(response => {
+      reportStatus(`Successfully added trip reservation`);
+      // Not sure what else to do
+    })
+    .catch(error => {
+      reportStatus(
+        `Encountered an error while reserving trip: ${error.message}`
+      );
+      console.log(error);
+    });
+};
 // How do we get details of trip they click on?
 // We have a url for the get request but how do we use that/get the id?
-function getTripData(trip) {
+const getTripData = trip => {
   // console.log(trip);
   // console.log(trip.id);
   let URL = `https://trektravel.herokuapp.com/trips/${trip.id}`;
@@ -80,14 +115,13 @@ function getTripData(trip) {
       reportStatus(`Encountered an error while loading trip: ${error.message}`);
       console.log(error);
     });
-}
+};
 $(document).ready(() => {
   $("#load").click(loadTrips);
   $("#trip-list").on("click", "li", function(event) {
     // Perform get request
     getTripData(this);
-    // console.log(event);
-    // Get trip data with id via get request (store in another function and call here?)
-    // Show trip details in a new section
+    // Show trip form as well in #reservation section
+    showReservationForm(this);
   });
 });
