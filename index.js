9924fbe5e5426aff97ae09a46f7f47d13e55b682
Amanda Ungco <amanda.ungco@gmail.com>
2018-11-21 09:26:05 -0800

    this is a poor commit message bc i didnt commit lat night and forgot the last change i made

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ const loadTrips = () => {
     .then((response) => {
       reportStatus(`Successfully loaded ${response.data.length} trips`);
       response.data.forEach((trip) => {
-        tripList.append(`<li>${trip.name}</li>`);
+        tripList.append(`<li id= ${trip.id}> ${trip.name}</li>`);
+        console.log(tripList)
       });
     })
     .catch((error) => {
@@ -39,64 +40,108 @@ const loadTrips = () => {
       console.log(error);
     });
 };
-
+//  click event -- acton show details
 //
-// Creating Trips
-//
-const readFormData = () => {
-  const parsedFormData = {};
+//View Individual Trip Details
+const loadTripDetails = (id) => {
 
-  const nameFromForm = $(`#trip-form input[name="name"]`).val();
-  parsedFormData['name'] = nameFromForm ? nameFromForm : undefined;
+  console.log(event)
+  reportStatus('Loading trip details...');
 
-  const ageFromForm = $(`#trip-form input[name="age"]`).val();
-  parsedFormData['age'] = ageFromForm ? ageFromForm : undefined;
+  const tripDetails = $('#trip-details');
+  tripDetails.empty();
 
-  const ownerFromForm = $(`#trip-form input[name="owner"]`).val();
-  parsedFormData['owner'] = ownerFromForm ? ownerFromForm : undefined;
-
-  return parsedFormData;
-};
-
-const clearForm = () => {
-  $(`#trip-form input[name="name"]`).val('');
-  $(`#trip-form input[name="age"]`).val('');
-  $(`#trip-form input[name="owner"]`).val('');
-}
-
-const createTrip = (event) => {
-  // Note that createTrip is a handler for a `submit`
-  // event, which means we need to call `preventDefault`
-  // to avoid a page reload
-  event.preventDefault();
-
-  const tripData = readFormData();
-  console.log(tripData);
-
-  reportStatus('Sending trip data...');
-
-  axios.post(URL, tripData)
+let tripUrl =  `https://trektravel.herokuapp.com/trips/${id}`
+  axios.get(tripUrl)
     .then((response) => {
-      reportStatus(`Successfully added a trip with ID ${response.data.id}!`);
-      clearForm();
+      console.log(response.data)
+        reportStatus(`Successfully loaded ${response.data.name} trip`)
+      const details  = Object.entries(response.data)
+      for (const [key, value] of details){
+        tripDetails.append(`<li>${key}: ${value}</li>`);
+      // });
+    }
+      $("p.trip-name").append(` <strong>${response.data.name}</strong>`);
     })
     .catch((error) => {
-      console.log(error.response);
-      if (error.response.data && error.response.data.errors) {
-        reportError(
-          `Encountered an error: ${error.message}`,
-          error.response.data.errors
-        );
-      } else {
-        reportStatus(`Encountered an error: ${error.message}`);
-      }
+      reportStatus(`Encountered an error while loading trips: ${error.message}`);
+      console.log(error);
     });
 };
+
+
+const tripData = {};
+tripData["value"] = 5;
+
+axios.post(URL, tripData)
+.then((response) => {
+
+})
+.catch((error) => {
+
+});
+// // Creating Trips
+// //
+// const readFormData = () => {
+//   const parsedFormData = {};
+//
+//   const nameFromForm = $(`#trip-form input[name="name"]`).val();
+//   parsedFormData['name'] = nameFromForm ? nameFromForm : undefined;
+//
+//   const ageFromForm = $(`#trip-form input[name="age"]`).val();
+//   parsedFormData['age'] = ageFromForm ? ageFromForm : undefined;
+//
+//   const ownerFromForm = $(`#trip-form input[name="owner"]`).val();
+//   parsedFormData['owner'] = ownerFromForm ? ownerFromForm : undefined;
+//
+//   return parsedFormData;
+// };
+//
+// const clearForm = () => {
+//   $(`#trip-form input[name="name"]`).val('');
+//   $(`#trip-form input[name="age"]`).val('');
+//   $(`#trip-form input[name="owner"]`).val('');
+// }
+
+// const createTrip = (event) => {
+//   // Note that createTrip is a handler for a `submit`
+//   // event, which means we need to call `preventDefault`
+//   // to avoid a page reload
+//   event.preventDefault();
+//
+//   const tripData = readFormData();
+//   console.log(tripData);
+//
+//   reportStatus('Sending trip data...');
+//
+//   // axios.post(URL, tripData)
+//   //   .then((response) => {
+//   //     reportStatus(`Successfully added a trip with ID ${response.data.id}!`);
+//   //     clearForm();
+//   //   })
+//   //   .catch((error) => {
+//   //     console.log(error.response);
+//   //     if (error.response.data && error.response.data.errors) {
+//   //       reportError(
+//   //         `Encountered an error: ${error.message}`,
+//   //         error.response.data.errors
+//   //       );
+//   //     } else {
+//   //       reportStatus(`Encountered an error: ${error.message}`);
+//   //     }
+//   //   });
+// };
 
 //
 // OK GO!!!!!
 //
 $(document).ready(() => {
+  $(".individual-trip").hide()
   $('#load').click(loadTrips);
+  $('#trip-list').on('click', 'li', function(event){
+    $(".individual-trip").show()
+    loadTripDetails(event.target.id);
+    console.log(event.target.id)
+  });
   $('#trip-form').submit(createTrip);
 });
