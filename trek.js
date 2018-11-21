d644a34d9052cdd089886e5d0b634a4601fa3e04
Sigrid Benezra <sdbenezra@gmail.com>
2018-11-20 23:14:36 -0800

    Css & bootstrap tweaks to trip list

--- trek.js
+++ trek.js
@@ -11,10 +11,11 @@ const listTrips = () => {
 
   .then((response) => {
     reportStatus(`Successfully loaded ${response.data.length} trips`);
+    tripList.append('<h2 class="card-title list-group-item">All Trips</h2>')
 
     response.data.forEach((trip) => {
     console.log(trip);
-    tripList.append(`<li>${trip.name}</li>`);
+    tripList.append(`<button class="list-group-item list-group-item-action">${trip.name}</button>`);
     });
   })
   .catch((error) => {
