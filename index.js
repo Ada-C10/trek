629ba89b48613953b2b850105b7e4dcd5e1b9171
jackie <jacquelyn.cheng@me.com>
2018-11-20 21:01:39 -0800

    Improved alert styling

--- index.js
+++ index.js
@@ -4,28 +4,28 @@ const inputs = ['name', 'email', 'trip', 'id'];
 
 const tripDetails = $('section.trip-details');
 const reserveTrip = $('section.reserve-trip');
+const alert = $('#status-message');
 
-const reportStatus = (message) => {
-  $('#status-message').html(message);
+const reportStatus = (message, type) => {
+  alert.html(message);
+  alert.removeClass().addClass(`alert alert-${type}`);
 };
 
 const loadTrips = () => {
-  reportStatus('Loading trips...');
-
-  // Prep work
   const tripList = $('#trip-list');
   tripList.empty();
 
-  // Actually load the trips
+  reportStatus('Loading trips...');
+
   axios.get(URL)
     .then((response) => {
-      reportStatus(`Successfully loaded ${response.data.length} trips`);
+      reportStatus(`Successfully loaded ${response.data.length} trips`, 'success');
       response.data.forEach((trip) => {
         tripList.append(`<li><a href="#" data-trip-id="${trip.id}">${trip.name}</a></li>`);
       });
     })
     .catch((error) => {
-      reportStatus(`Encountered an error while loading trips: ${error.message}`);
+      reportStatus(`Encountered an error while loading trips: ${error.message}`, 'danger');
       console.log(error);
     });
 };
@@ -36,7 +36,7 @@ const loadDetail = (event) => {
   const tripID = $(event.currentTarget).attr('data-trip-id');
   const tripDetail = $('#trip-detail');
 
-  reportStatus('Loading trip details...');
+  reportStatus('Loading trip details...', 'warning');
 
   // Prep work
   tripDetail.empty();
@@ -44,8 +44,6 @@ const loadDetail = (event) => {
   // Load the details
   axios.get(URL + tripID)
     .then((response) => {
-      reportStatus(`Successfully loaded ${response.data.name}`);
-      console.log(response.data);
       const tripInfo = ['name', 'continent', 'category', 'weeks', 'cost', 'about']
       for (let info of tripInfo) {
         tripDetail.append(`<li><span class="capitalize">${info}</span>: ${response.data[info]}</li>`);
@@ -55,9 +53,11 @@ const loadDetail = (event) => {
       $(`#reservation-form input[name="trip"]`).val(response.data.name);
       $(`#reservation-form input[name="id"]`).val(response.data.id);
       reserveTrip.show();
+
+      reportStatus(`Successfully loaded ${response.data.name}`, 'success');
     })
     .catch((error) => {
-      reportStatus(`Encountered an error while loading trips: ${error.message}`);
+      reportStatus(`Encountered an error while loading trips: ${error.message}`, 'danger');
       console.log(error);
     });
 };
@@ -85,22 +85,22 @@ const createReservation = (event) => {
   const reservationData = readFormData();
   const tripID = reservationData.id;
 
-  reportStatus('Sending reservation data...');
+  reportStatus('Sending reservation data...', 'warning');
 
   axios.post(URL + tripID + '/reservations', reservationData)
     .then((response) => {
-      reportStatus(`Successfully added a reservation for trip ${reservationData.trip}!`);
+      reportStatus(`Successfully added a reservation for trip ${reservationData.trip}!`, 'success');
       clearForm();
     })
     .catch((error) => {
       console.log(error.response);
       if (error.response.data && error.response.data.errors) {
         reportError(
-          `Encountered an error: ${error.message}`,
+          `Encountered an error: ${error.message}, 'danger'`,
           error.response.data.errors
         );
       } else {
-        reportStatus(`Encountered an error: ${error.message}`);
+        reportStatus(`Encountered an error: ${error.message}, 'danger'`);
       }
     });
 };
