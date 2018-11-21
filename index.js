22dede9d1d1161ff5a9e8b5fd154afdb1381a2e9
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-11-21 10:20:02 -0800

    show details for clicked trip displaying on page

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// const getEscape = () => {
+// //
+// // }
+
 const loadEscapes = () => {
 
   // list.empty() - do I need this?
@@ -8,7 +12,25 @@ const loadEscapes = () => {
         .then((response) => {
           escapeList.prepend(`<h2>Quick! Get out of here! </h2>`);
           response.data.forEach((escape) => {
-            escapeList.append(`<li>${escape.name}</li>`);
+            const $tripItem = $(`<li>${escape.name}</li>`);
+
+              $tripItem.click(() => {
+                // console.log(escape.id)
+                axios.get(getUrl + '/' + escape.id)
+                  .then((response) => {
+                    console.log(response.data)
+                    let escape = response.data
+                    $('.detail-list').prepend(`<h1>${escape.name}</h1>`)
+
+                    $('.detail-list').append(`<li><b>Continent:</b> ${escape.continent}</li>
+                    <li><b>Category:</b> ${escape.category}</li>
+                    <li><b>Weeks:</b> ${escape.weeks}</li>
+                    <li><b>Cost:</b> ${escape.cost}</li>
+                    <li><b>About:</b> ${escape.about}</li>`)
+                  })
+              })
+
+            escapeList.append($tripItem);
           })
           $( "#load" ).remove();
         })
@@ -23,9 +45,11 @@ $(document).ready( function() {
   // load all escapes
   $('#load').click(loadEscapes);
 
-  // $('#escape-list').on('click', 'li', function() {
-  //     // code to make the show details for a trip to appear on page
-  //   })
+  $('#escape-list').on('click', 'li', function() {
+      console.log($(this).text())
+      console.log($(this).html())
+      // code to make the show details for a trip to appear on page
+    })
 
 
 });
