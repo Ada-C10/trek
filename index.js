d1700e2b3b689755500c80fb81e68f63f57e49dd
Hannah <hannahvanseeters@gmail.com>
2018-11-20 19:40:50 -0800

    added individual trip details with data-id added like class in all trips listing

--- index.js
+++ index.js
@@ -17,27 +17,65 @@ const reportError = (message, errors) => {
   reportStatus(content);
 };
 
-const loadTrips = () => {
-  reportStatus('Loading trips...');
-
-  const tripList = $('.list-trips');
-  tripList.empty();
-
-  axios.get(allTripURL)
-    .then((response) => {
-      reportStatus(`Successfully loaded ${response.data.length} trips`);
-      response.data.forEach((trip) => {
-        tripList.append(`<p>${trip.name}</p>`);
-      });
-    })
-    .catch((error) => {
-      reportStatus(`Encountered an error while loading trips: ${error.message}`);
-      console.log(error);
-    });
-};
 
 //  READY GO!
 $(document).ready(() => {
+
+  const loadTrips = () => {
+    reportStatus('Loading trips...');
+
+    const tripList = $('.list-trips');
+    tripList.empty();
+   // trip id attached as class to look up individual trips?
+    axios.get(allTripURL)
+      .then((response) => {
+        reportStatus(`Successfully loaded ${response.data.length} trips`);
+        response.data.forEach((trip) => {
+          tripList.append(`<p data-id="${trip.id}">${trip.name}</p>`);
+        });
+      })
+      .catch((error) => {
+        reportStatus(`Encountered an error while loading trips: ${error.message}`);
+        console.log(error);
+      });
+  };
+
+    // INDIVIDUAL TRIP DEAILS
+    const loadTrip = function loadTrip(id) {
+      reportStatus('Loading trip...');
+
+      const tripDetails = $('.trip-details');
+      tripDetails.empty();
+     // trip id attached as class to look up individual trips?
+      axios.get(`${allTripURL}/${id}`)
+        .then((response) => {
+          reportStatus(`Successfully loaded ${response.data.name}`);
+          let details = $(`<h2>${response.data.name}</h2>
+                          <p>ID: ${response.data.id}</p>
+                          <p>Continent: ${response.data.continent}</p>
+                          <p>About: ${response.data.about}</p>
+                          <p>Category: ${response.data.category}</p>
+                          <p>Weeks: ${response.data.weeks}</p>
+                          <p>Cost: ${response.data.cost}</p>`)
+
+        tripDetails.append(details);
+            // tripList.append(`<href class="${trip.id}">${trip.name}</p>`);
+
+        })
+        .catch((error) => {
+          reportStatus(`Encountered an error while loading trips: ${error.message}`);
+          console.log(error);
+        });
+    };
+
+
+  // all trips button
   $('.load-trips').click(loadTrips);
+  // one trip details
+  $('.list-trips').on('click', 'p', function() {
+    const id = $(this).attr('data-id');
+    loadTrip(id);
+  });
+
   // $('#pet-form').submit(createPet);
 });
