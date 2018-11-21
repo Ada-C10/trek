4d9d3baf3eb1b35247f6668236cda8238b283e28
Lindsay Terchin <lterchin@gmail.com>
2018-11-20 20:24:36 -0800

    created function to get all trips

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const URL = 'https://trektravel.herokuapp.com/';
+const URL = 'https://trektravel.herokuapp.com/trips';
+
+
+const loadTrips = () => {
+  const tripList = $('#trip-list');
+
+  axios.get(URL)
+  .then((response) => {
+    response.data.forEach((trip) => {
+      tripList.append(`<li>${trip.name}</li>`);
+    });
+
+  });
+};
+
+
+$(document).ready(() => {
+  $('#trips').click(loadTrips);
+});
