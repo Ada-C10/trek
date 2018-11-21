f89f20ebf0696833df1cfb6ec61f260d868561e7
Lindsay Terchin <lterchin@gmail.com>
2018-11-21 14:49:19 -0800

    added individual trip URLs to each trip

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ const loadTrips = () => {
   .then((response) => {
     reportStatus(`Successfully loaded ${response.data.length} trips`);
     response.data.forEach((trip) => {
-      tripList.append(`<li><a href = 'http://www.google.com'>${trip.name}</a></li><li>${trip.id}`);
+      let tripId = trip.id;
+      let tripUrl = `https://trektravel.herokuapp.com/trips/${tripId}`;
+      tripList.append(`<li><a href = ${tripUrl}>${trip.name}</a></li><li>${trip.id}</li><li>${tripUrl}</li>`);
     });
   })
   .catch((error) => {
@@ -33,6 +35,21 @@ const loadTrips = () => {
     console.log(error);
   });
 };
+
+// const singleTrip = () => {
+//   let id = 1;
+//   let URL2 = `${URL}/${id}`;
+//   const tripInfo = $('#trip-info');
+//
+//   axios.get(URL2)
+//   .then((response) => {
+//     reportStatus(`trip data successfully loaded`);
+//     response.data
+//
+//   })
+//
+//
+// };
 
 
 $(document).ready(() => {
