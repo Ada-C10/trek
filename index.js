acdaf431af66cc76ad4f6a404c01329032fbc491
Semret Nicodimos <snicodimos@gmail.com>
2018-11-20 16:38:57 -0800

    added css html and js files with skeleton codes set up

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const BaseURL = 'https://trektravel.herokuapp.com/trips';
+
+const reportStatus = (message) => {
+  $('#status-message').html(message);
+};
+
+const loadTrips = () => {
+  const tripList = $('#travel-list')
+
+
+
+};
+
+
+
+$(document).ready(()=> {
+
+});
