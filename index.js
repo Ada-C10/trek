abcd357b543074fe3e3d56bf069ba48d719dbf59
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-11-20 16:08:13 -0800

    Added error reporting

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 // index.js
 const URL = "https://trektravel.herokuapp.com/trips";
 
+const reportStatus = message => {
+  $("#status-message").html(message);
+};
+
+const reportError = (message, errors) => {
+  let content = `<p>${message}</p><ul>`;
+  for (const field in errors) {
+    for (const problem of errors[field]) {
+      content += `<li>${field}: ${problem}</li>`;
+    }
+  }
+  content += "</ul>";
+  reportStatus(content);
+};
+
 const loadTrips = () => {
+  reportStatus("Loading trips...");
   // Prep work
   const tripList = $("#trip-list");
   tripList.empty();
@@ -11,17 +27,28 @@ const loadTrips = () => {
     .get(URL)
     .then(response => {
       // .then runs if call succeeds
+      reportStatus(`Successfully loaded ${response.data.length} trips`);
       response.data.forEach(trip => {
-        tripList.append(`<li>${trip.name}</li>`);
+        tripList.append(`<li class="trip">${trip.name}</li>`);
       });
     })
     // .catch is called on return value of .then()
     .catch(error => {
       //.catch runs if call fails
-      console.log(error);
+      reportStatus(`Encountered an error while loading pets: ${error.message}`);
     });
 };
-
+// Showing a trip details on homepage
+let showTrip = trip => {
+  // Show a trip via this?
+};
 $(document).ready(() => {
   $("#load").click(loadTrips());
+  // If a trip name is clicked, append a new section with trip details
+  $(".trip").click(function(event) {
+    // Append trip info
+    console.log(event);
+    // Make helper method to create a nice looking trip ordered list?
+    $("#trip-details").append("TEST");
+  });
 });
