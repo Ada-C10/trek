b20ec1fec35c9502a38642904ee414a44e882788
Dionisia Montanez <dmontanez15@gmail.com>
2018-11-20 21:16:34 -0800

    wave 2 done

--- index.js
+++ index.js
@@ -6,17 +6,48 @@ const reportStatus = (message) => {
 
 const loadTrips = () => {
   // Prep work
-  const tripList = $('#trip-list');
+  const tripList = $('#trips');
   // if this .empty is not here, it will keep appending more and more trips to the list.
   tripList.empty();
 
   // Actually load the trips
+  // const $tripList = $('#trip-list')
+
   axios.get(URL)
   .then((response) => {
-    console.log(response);
+    // console.log(response);
     reportStatus(`Successfully loaded ${response.data.length} trips`)
-    response.data.forEach((trip) => {
-      tripList.append(`<li>${trip.name}</li>`);
+    response.data.slice(0, 10).forEach((trip) => {
+      // const $tripItem = $(`<li id="trip-${trip.id}">${trip.name}</li>`);
+      const $tripItem = $(`<li>${trip.name}</li>`);
+
+      // console.log($tripItem);
+      tripList.append($tripItem);
+
+      $tripItem.click((event) => {
+        // console.log(event.target.id);
+        // const tripId = event.target.id.split('-')[1];
+        // console.log(tripId);
+        axios.get(URL + '/' + trip.id)
+          .then((response) => {
+            const trip = response.data;
+
+            $('body').append(`
+                <div class="trip-info">
+                  <p>${trip.name}</p>
+                  <p>${trip.continent}</p>
+                  <p>${trip.about}</p>
+                  <p>${trip.category}</p>
+                  <p>${trip.weeks}</p>
+                  <p>${trip.cost}</p>
+                </div>`);
+          });
+      });
+      // const tripList = (`<li>${trip.name}</li>`);
+
+      // tripList.append(`<li><a href="https://trektravel.herokuapp.com/trips/${trip.id}">${trip.name}</li>`);
+
+
     });
   })
 
@@ -27,9 +58,11 @@ const loadTrips = () => {
   });
 };
 
+// const getTripDetail () => {
+//   // const tripList = $('#trip-list');
+//
+// }
 
 $(document).ready(() => {
   $('#load').click(loadTrips);
-  // loadPets();
-  // $('#pet-form').submit(createPet);
 });
