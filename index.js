5e1cf7a84afc7ac91cf397fc227b24eb9854e85e
Jane <jane.edwards.mcnear@gmail.com>
2018-11-20 17:55:34 -0800

    added links to show trip details

--- index.js
+++ index.js
@@ -5,16 +5,31 @@ const reportStatus = (message) => {
 const loadTrips = () => {
   const URL = `https://trektravel.herokuapp.com/trips`;
   const tripList = $('#trip-list');
+  tripList.empty();
   axios.get(URL)
   .then((response) => {
     response.data.forEach((trip) => {
-      tripList.append(`<li>${trip.name}</li>`);
+      tripList.append(`<li><a href="#" id="${trip.id}">${trip.name}</a></li>`);
     });
     reportStatus(`Successfully loaded all trips`);
-
+  });
+};
+const showTrip = (id) => {
+  const URL = `https://trektravel.herokuapp.com/trips/`;
+  const tripShowList = $('#trip-details');
+  tripShowList.empty();
+  axios.get(URL + id)
+  .then((response) => {
+    for (let tripData in response.data) {
+      tripShowList.append(`<li><strong>${tripData}</strong>: ${response.data[tripData]}</li>`);
+    }
+    reportStatus(`Successfully loaded all trips`);
   });
 };
 
 $(document).ready(() => {
   $('#load').click(loadTrips);
+  $('#trip-list').on('click', 'a', function() {
+    showTrip(this.id);
+  });
 });
