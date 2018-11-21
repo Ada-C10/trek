7e9304cfebe7d2905a9a5b9d9a2422cea9756336
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-11-21 15:31:03 -0800

    Continued work on reserving a trip. Decided to show trip name as a read only field/to have a hidden id field

--- index.js
+++ index.js
@@ -30,60 +30,129 @@ const loadTrips = () => {
       reportStatus(`Successfully loaded ${response.data.length} trips`);
       response.data.forEach(trip => {
         tripList.append(`<li class="trip", id=${trip.id}>${trip.name}</li>`);
-        // trip.click(function() {
-        //   // TODO Perform get request to get trip data
-        //   // TODO Append trip details to new section
-        // });
       });
     })
     // .catch is called on return value of .then()
     .catch(error => {
-      //.catch runs if call fails
-      reportStatus(`Encountered an error while loading pets: ${error.message}`);
+      if (error.response.data && error.response.data.errors) {
+        reportError(
+          `Encountered an error: ${error.message}`,
+          error.response.data.errors
+        );
+      } else {
+        reportStatus(`Encountered an error: ${error.message}`);
+      }
     });
 };
-const form =
-  `<div class="container">` +
-  `<form id=trip-form>` +
-  `<label for="name">` +
-  `<input type="text" name="name">` +
-  "Name" +
-  `</input>` +
-  `</label>` +
-  `<input type="submit" name="add-reservation" value="Complete Reservation">` +
-  `</form>` +
-  `</div>`;
-const showReservationForm = trip => {
-  // Appends the form
-  $("#reservationForm").append(form);
-  // $("#reservationForm").append("<label for="name">Name</label>");
-  // $("#reservationForm").append("</div>");
+
+const showReservationForm = event => {
+  const tripForm =
+    `<h1>Reserve Trip</h1>` +
+    `<form id=trip-form>` +
+    `<label for="name">` +
+    `</label>` +
+    `<input type="text" name="name">` +
+    "Your Name" +
+    `</input>` +
+    `<label for="email">` +
+    `</label>` +
+    `<input type="text" name="email">` +
+    "Email" +
+    `</input>` +
+    `</input>` +
+    `<label for="Trip Name">` +
+    `</label>` +
+    `<input type="text" name="tripName" value="(${
+      event.textContent
+    })" readonly>` +
+    `</input>` +
+    `</input>` +
+    `<label for="tripID">` +
+    `</label>` +
+    `<input type="text" name="tripID" value="${event.id}" hidden>` +
+    `</input>` +
+    `<input type="submit" name="add-reservation" value="Complete Reservation">` +
+    `</input>` +
+    `</form>`;
+  // Add trip name to tripForm?
+  // Appends the form the id as hidden/the name as read only
+  // $("#trip-form").append(
+  //   `<input type="hidden" name="tripID" value="${trip.id}" readonly>`
+  // );
+  // Add trip name to field as read only
+  // $("#trip-form").append(
+  //   $("<input>").attr({
+  //     type: "text",
+  //     name: "Trip Name",
+  //     value: `${event.textContent}`,
+  //     disabled: true
+  //   }),
+  //   $("<input>").attr({
+  //     type: "text",
+  //     name: "TripID",
+  //     value: `${event.id}`,
+  //     hidden: true
+  //   })
+  // `<input type="text" name="tripName value="${trip.name} readonly"></input>`
+  // );
+  $("#reservation").append(tripForm);
 };
-const createReservation = trip => {
-  // trip.preventDefault()?
-  // Says what to do with form submit
-  // Name, email, trip name (pulled in from what they clicked on?),
-  // Requires name and email, can also accept age
-  let URL = `https://trektravel.herokuapp.com/trips/${trip.id}/reservations`;
+const readFormData = () => {
+  const parsedFormData = {};
+  const emailFromForm = $(`#trip-form input[name="email"]`).val();
+  parsedFormData["email"] = emailFromForm ? emailFromForm : undefined;
+  const nameFromForm = $(`#trip-form input[name="name"]`).val();
+  parsedFormData["name"] = nameFromForm ? nameFromForm : undefined;
+  const tripNameFromForm = $(`#trip-form input[name="tripName"]`).val();
+  parsedFormData["tripName"] = tripNameFromForm ? tripNameFromForm : undefined;
+  const idFromForm = $(`#trip-form input[name="tripID"]`).val();
+  parsedFormData["tripID"] = idFromForm ? idFromForm : undefined;
+
+  // console.log(parsedFormData);
+  return parsedFormData;
+};
+
+const clearForm = () => {
+  $(`#trip-form input[name="name"]`).val("");
+  $(`#trip-form form input[name="email"]`).val("");
+  $(`#trip-form form input[name="tripID"]`).val("");
+  $(`#trip-form form input[name="tripName"]`).val("");
+};
+const createReservation = event => {
+  event.preventDefault();
+  // TODO TODO QUESTION trip.preventDefault()? QUESTION
+  // QUESTION How do I get id over here? QUESTION
+  console.log(event);
+  event.preventDefault();
+  const tripData = readFormData();
+  let URL = `https://trektravel.herokuapp.com/trips/${
+    tripData.tripID
+  }/reservations`;
+  reportStatus("Creating reservation...");
   axios
-    .post(URL)
+    .post(URL, tripData)
     .then(response => {
-      reportStatus(`Successfully added trip reservation`);
-      // Not sure what else to do
+      reportStatus(
+        `Successfully added trip reservation with id ${tripData.tripID}`
+      );
+      clearForm();
     })
     .catch(error => {
-      reportStatus(
-        `Encountered an error while reserving trip: ${error.message}`
-      );
-      console.log(error);
+      if (error.response.data && error.response.data.errors) {
+        reportError(
+          `Encountered an error: ${error.message}`,
+          error.response.data.errors
+        );
+      } else {
+        reportStatus(`Encountered an error: ${error.message}`);
+      }
     });
 };
 // How do we get details of trip they click on?
 // We have a url for the get request but how do we use that/get the id?
-const getTripData = trip => {
-  // console.log(trip);
-  // console.log(trip.id);
-  let URL = `https://trektravel.herokuapp.com/trips/${trip.id}`;
+const getTripData = event => {
+  console.log(event);
+  let URL = `https://trektravel.herokuapp.com/trips/${event.id}`;
   axios
     .get(URL)
     .then(response => {
@@ -102,14 +171,12 @@ const getTripData = trip => {
 
       // Append to trip-details section
       $("#trip-details").append("<h1>Details</h1>");
+
       // For each in parsedTripData
       for (let detail in parsedTripData) {
         $("#trip-details").append(`<h2>${detail}</h2>`);
         $("#trip-details").append(`<p>${parsedTripData[detail]}`);
       }
-      // response.data.forEach(pet => {
-      //   petList.append(`<li>${pet.name}</li>`);
-      // });
     })
     .catch(error => {
       reportStatus(`Encountered an error while loading trip: ${error.message}`);
@@ -120,8 +187,11 @@ $(document).ready(() => {
   $("#load").click(loadTrips);
   $("#trip-list").on("click", "li", function(event) {
     // Perform get request
+    console.log(this);
     getTripData(this);
     // Show trip form as well in #reservation section
     showReservationForm(this);
+    // Submit form on click
+    $("#trip-form").submit(createReservation);
   });
 });
