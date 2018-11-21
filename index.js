4f1b37bf429fb84e5aaf023ab30ed0c643b7674f
Alice Hsiao <alice.hsiao87@gmail.com>
2018-11-20 21:33:40 -0800

    Created ability to see trip details

--- index.js
+++ index.js
@@ -1,21 +1,64 @@
-const URL = "https://trektravel.herokuapp.com/trips";
+const INDEX_URL = "https://trektravel.herokuapp.com/trips";
+
+const parsedTripDetails = (response) => {
+  const div = $('<div></div>');
+  div.append(`Trip ID: ${response.data.id}<br />`);
+  div.append(`Trip Name: ${response.data.name}<br />`);
+  div.append(`Continent: ${response.data.continent}<br />`);
+  div.append(`Category: ${response.data.category}<br />`);
+  div.append(`Weeks: ${response.data.weeks}<br />`);
+  div.append(`Cost: ${response.data.cost}<br />`);
+  div.append(`Description: ${response.data.about}`);
+  return div;
+}
+
+const clearForm = () => {
+  $("#form")[0].reset();
+}
+
+const createForm = (event) => {
+  const form = $('<form></form>');
+  form.attr("id", "trip-form");
+
+  const options = ["name", "email", "trip-name"];
+  for (let option of options){
+    const div = $('<div></div>');
+    div.append(`<label for="${option}">${option}</label>`);
+    if(option === "trip-name"){
+      div.append(`<input type="text" name="${option}" class="${option}" value="${$(event.target).text()}"/>`);
+    } else {
+      div.append(`<input type="text" name="${option}" class="${option}"/>`);
+    }
+    form.append(div);
+  }
+
+  form.append(`<input type="submit" name="add-trip" value="Add Trip" />`);
+
+  return form;
+}
+
 // const reportStatus = (message) => {
 //   $('#status-message').html(message);
 // };
 
 const loadTrips = () => {
-  const div = $('<div></div>');
-  div.addClass('col-md-6');
-  const tripList = $('#trip');
+  const tripList = $('#trips');
   tripList.empty();
+  tripList.append('<h4>All Trips</h4>');
 
-  axios.get(URL)
+  const ul = $('<ul></ul>');
+
+  axios.get(INDEX_URL)
     .then((response) => {
-      div.append('<h4>All Trips</h4>')
       response.data.forEach((trip) => {
-        div.append(`<li>${trip.name}</li>`);
+        const li = $('<li></li>');
+        const a = $(`<a>${trip.name}</a>`);
+        a.attr('href', `https://trektravel.herokuapp.com/trips/${trip.id}`);
+        a.data('id', trip.id);
+        li.append(a);
+        ul.append(li);
       });
-      tripList.append(div);
+      tripList.append(ul);
       // reportStatus(`Successfully loaded ${sevenWonders.length} wonders`);
     })
     .catch((error) => {
@@ -23,6 +66,31 @@ const loadTrips = () => {
     });
 };
 
+const loadTripDetails = (event) => {
+  event.preventDefault();
+  const url = $(event.target)[0].href;
+  // const id = $(event.target).data("id");
+
+  const tripDetails = $('#trip');
+  tripDetails.empty();
+  tripDetails.append('<h4>Details</h4>');
+
+  axios.get(url)
+    .then((response) => {
+      tripDetails.append(parsedTripDetails(response));
+      // reportStatus(`Successfully loaded ${sevenWonders.length} wonders`);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  const form = $('#form');
+  form.empty();
+  form.append('<h4>Reserve Today!</h4>');
+  form.append(createForm(event));
+}
+
 $(document).ready(() => {
   $('#load').click(loadTrips);
+  $('#trips').on('click', loadTripDetails);
 });
