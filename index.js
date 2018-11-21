d68337c57aedf7505bd8f30fd300a64a4331fa11
BarbaraWidjono <barbarawidjono@gmail.com>
2018-11-20 16:08:26 -0800

    Display list of all trips

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ const reportStatus = (message) => {
   $('#status-message').html(message);
 };
 
+const loadTrips = () => {
+  reportStatus("loading all trips...");
+
+  const tripList = $('#tripList');
+  tripList.empty();
+
+  axios.get(URL)
+  .then((response) => {
+    // console.log("In the axios.get => .then method");
+    response.data.forEach((trip) => {
+      // console.log(trip.name);
+      tripList.append(`<li>${trip.name}</li>`);
+    })
+  })
+
+  .catch((error) => {
+    console.log("In the axios.get => .catch method");
+  })
+}
+
+
+
+
+
 $(document).ready(() => {
   alert("jQuery Works");
 
