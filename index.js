33b1404d763f50dde7f648c982997e7fb372f447
Lindsay Terchin <lterchin@gmail.com>
2018-11-21 14:06:29 -0800

    added report status & errors to page

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const URL = 'https://trektravel.herokuapp.com/trips';
 
 
+const reportStatus = (message) => {
+  $('#status-message').html(message);
+};
+
+const reportError = (message, errors) => {
+  let content = `<p>${message}</p><ul>`;
+  for (const field in errors) {
+    for (const problem of errors[field]) {
+      content += `<li>${field}: ${problem}</li>`;
+    }
+  }
+  content += "</ul>";
+  reportStatus(content);
+};
+
+
 const loadTrips = () => {
+  reportStatus('Loading trips...');
   const tripList = $('#trip-list');
 
   axios.get(URL)
   .then((response) => {
+    reportStatus(`Successfully loaded ${response.data.length} trips`);
     response.data.forEach((trip) => {
-      tripList.append(`<li>${trip.name}</li>`);
+      tripList.append(`<li>${trip.name}</li><li>${trip.id}`);
     });
-
+  })
+  .catch((error) => {
+    reportStatus(`Encountered an error: ${error.message}`);
+    console.log(error);
   });
 };
 
