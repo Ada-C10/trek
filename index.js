125ca7953ea1be3001122d171d25d5a0406c3e0a
Trang <trang.frego@gmail.com>
2018-11-21 21:43:32 -0800

    create getTripDetails and reserveTrip

--- index.js
+++ index.js
@@ -25,8 +25,9 @@ const loadTrips = () => {
     .then((response) => {
       console.log(response);
       reportStatus(`Successfully loaded ${response.data.length} trips`);
-      response.data.forEach((trip) => {
-        tripList.append(`<li><a href="#">${trip.name}</a></li>`);
+      tripList.append('<h2>All Trips</h2>')
+      response.data.forEach( (trip) => {
+        tripList.append(`<li id="${trip.id}"><a href="#">${trip.name}</a></li>`);
       });
     })
     .catch((error) => {
@@ -38,18 +39,102 @@ const loadTrips = () => {
 const getTripDetails = (trip) => {
   reportStatus('Retrieving trip details...');
 
-  const trip = $('#trip-details');
-  trip.empty();
+  const tripDetails = $('#trip-details');
+  tripDetails.empty();
 
 
+  axios.get(URL + `/${trip.id}`)
+    .then((response) => {
+      tripDetails.append(`
+        <h2>Trip Details</h2>
+        <li>Name: ${response.data.name}</li>
+        <li>Continent: ${response.data.continent}</li>
+        <li>Category: ${response.data.category}</li>
+        <li>Weeks: ${response.data.weeks}</li>
+        <li>Cost: $${response.data.cost}</li>
+        <li>About: ${response.data.about}</li>`);
+    });
 
+};
+
+const getRezForm = (trip) => {
+  const rezForm = $('#rez-form')
+  rezForm.empty();
+
+  rezForm.append(`
+    <h2>Reserve Trip</h2>
+    <div>
+      <label for="name">Your name</label>
+      <input type="text" name="name" />
+    </div>
+    <div>
+      <label for="email">Email</label>
+      <input type="text" name="email" />
+    </div>
+    <div>
+      <input type="hidden" name="trip_id" value=${trip.id} />
+    </div>
+    <input type="submit" name="reserve" value="Reserve" />
+  `);
+
+}
+
+const readFormData = () => {
+  const parsedFormData = {};
+
+  const tripFromForm = $(`#rez-form input[name="trip_id"]`).val();
+  parsedFormData['trip_id'] = tripFromForm ? tripFromForm : undefined;
+
+  const nameFromForm = $(`#rez-form input[name="name"]`).val();
+  parsedFormData['name'] = nameFromForm ? nameFromForm : undefined;
+
+  const emailFromForm = $(`#rez-form input[name="email"]`).val();
+  parsedFormData['email'] = emailFromForm ? emailFromForm : undefined;
+
+  return parsedFormData;
+};
+
+const clearForm = () => {
+  $(`#rez-form input[name="name"]`).val('');
+  $(`#rez-form input[name="email"]`).val('');
+  $(`#rez-form input[name="trip"]`).val('');
+}
+
+const reserveTrip = (event) => {
+  console.log(event);
+  event.preventDefault();
+
+  const tripData = readFormData();
+  console.log(tripData);
+
+  reportStatus('Sending reservation data...');
+
+  axios.post(URL + '/' + tripData.trip_id + '/reservations', tripData)
+    .then((response) => {
+      reportStatus(`Successfully reserved a trip with ID ${response.data.id}!`);
+      clearForm();
+    })
+    .catch((error) => {
+      console.log(error.response);
+      if (error.response.data && error.response.data.errors) {
+        reportError(
+          `Encountered an error: ${error.message}`,
+          error.response.data.errors
+        );
+      } else {
+        reportStatus(`Encountered an error: ${error.message}`);
+      }
+    });
 };
 
 $(document).ready(() => {
   $('#load').click(loadTrips);
+
   $('#trip-list').on('click', 'li', function(trip) {
-    getTripDetails()
+    console.log(this.id);
+    getTripDetails(this);
+    getRezForm(this);
   });
 
-  // $('#trip-form').submit(createTrip);
+  $('#rez-form').submit(reserveTrip);
 });
