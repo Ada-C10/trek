e696c92fbdba6c49ed41d989828beae4623f047a
Karis Kim <ouniiq@hotmail.com>
2018-11-21 16:28:32 -0800

    made specifc trip detail to show when a trip is clicked

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const url =  "https://trektravel.herokuapp.com/trips";
+const treksUrl =  "https://trektravel.herokuapp.com/trips";
+const oneTrekUrl = "https://trektravel.herokuapp.com/trips/";
 
-const loadTreks = () => {
+
+
+const loadAllTreks = () => {
   // Prep work
   const trekList = $('#trek-list');
   trekList.empty();
 
   // Actually load the pets
-  axios.get(url)
+  axios.get(treksUrl)
     .then((response) => {
       response.data.forEach((trek) => {
-        trekList.append(`<li>${trek.name}</li>`);
+        const listItem = $(`<li class="${trek.id}"> ${trek.name}</li>`);
+
+        trekList.append(listItem);
+        // here is after we have loaded trips
+        // and added one entry to the list
+        // set up the click handler for that entry
+        listItem.on('click', () => {loadOneTrek(`${trek.id}`)});
       });
     })
     .catch((error) => {
@@ -17,6 +26,28 @@ const loadTreks = () => {
     });
 };
 
+const loadOneTrek = (id) => {
+  const trekNumber = oneTrekUrl + id;
+
+  axios.get(trekNumber)
+    .then((response) => {
+      $('#tttrek').empty();
+      const trekData = response.data
+        $('#tttrek').append(`<li> ${trekData.id}</li>`);
+        $('#tttrek').append(`<li> ${trekData.name}</li>`);
+        $('#tttrek').append(`<li> ${trekData.continent}</li>`);
+        $('#tttrek').append(`<li> ${trekData.about}</li>`);
+        $('#tttrek').append(`<li> ${trekData.category}</li>`);
+        $('#tttrek').append(`<li> ${trekData.weeks}</li>`);
+        $('#tttrek').append(`<li> ${trekData.cost}</li>`);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+
+
 $(document).ready(() => {
-  $('#load').click(loadTreks);
+  $('#load').click(loadAllTreks);
 });
