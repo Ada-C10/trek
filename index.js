1c60e3e1ddd0fd20709039e6622e9aef9dbcca4c
Anibel America <ameriabel@gmail.com>
2018-11-22 10:59:19 -0800

    adds show individual trip closure

--- index.js
+++ index.js
@@ -16,28 +16,61 @@ const reportError = (message, errors) => {
   reportStatus(content);
 };
 
+const showTrip = (response) => {
+  const target = $('#trip-details');
+  let details = `<h4>Name: ${response.name}</h4>`;
+  details += `<p>Continent: ${response.continent}</p>`;
+  details += `<p>Category: ${response.category}</p>`;
+  details += `<p>Weeks: ${response.weeks}</p>`;
+  details += `<p>Cost: $${response.cost}</p>`;
+  details += `<p>About:</p><br><p>${response.about}`;
+  target.append(details);
+};
+
+const buildTripCallback = (id) => {
+  const loadTrip = () => {
+
+    axios.get(URL+`/${id}`)
+      .then((response) => {
+        console.log(response.data);
+        reportStatus(`Successfully loaded trip ${id}.`);
+        showTrip(response);
+      })
+      .catch((error) => {
+        reportStatus(`Encountered an error while loading trips: ${error.message}`);
+        console.log(error);
+      });
+  }
+  return loadTrip;
+};
+
 const loadTrips = () => {
 
   const tripList = $('#all-trips');
   tripList.append('All Trips');
-  // tripList.empty();
 
   axios.get(URL)
     .then((response) => {
+      console.log(response);
       reportStatus(`Successfully loaded ${response.data.length} trips`);
-      console.log(response.data);
       response.data.forEach((trip) => {
-        // const row = $('li');
-        // row.addClass(trip.)
-        tripList.append(`<li>${trip.name}</li>`);
+        tripList.append(`<li class=${trip.id}>${trip.name}</li>`);
+        const tripClickHandler = buildTripCallback(trip.id);
+        $(`.${trip.id}`).click(tripClickHandler);
       });
     })
     .catch((error) => {
-      reportStatus(`Encountered an error while loading pets: ${error.message}`);
+      reportStatus(`Encountered an error while loading trips: ${error.message}`);
       console.log(error);
     });
 };
 
+const loadTripDetails = () => {
+
+
+};
+
 $(document).ready(() => {
   $('#see-trips-button').click(loadTrips);
+  $('#all-trips').on('click', 'li', loadTripDetails);
 });
