f4d7273423e8d4faa6aeea575d74a59f70ebc714
Katie Jahanmir <katietj@gmail.com>
2018-11-22 11:33:53 -0800

    completed make reservation funcitionality

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const reportStatus = (message) => {
   $('#status-message').html(message);
 };
 
+const reportError = (message, errors) => {
+  let content = `<p>${message}</p>`
+  content += "<ul>";
+  for (const field in errors) {
+    for (const problem of errors[field]) {
+      content += `<li>${field}: ${problem}</li>`;
+    }
+  }
+  content += "</ul>";
+  reportStatus(content);
+};
+
+
 const loadTrips = () => {
   reportStatus('Loading trips...');
 
@@ -12,7 +25,7 @@ const loadTrips = () => {
   const tripList = $('#trip-list');
   tripList.empty();
 
-  
+
   axios.get(URL)
     .then((response) => {
       response.data.forEach((trip) => {
@@ -26,6 +39,53 @@ const loadTrips = () => {
       reportStatus(`Encountered an error while loading trips: ${error.message}`);
     });
 };
+
+const readFormData = () => {
+  const parsedFormData = {};
+
+  const nameFromForm = $(`#form input[name="name"]`).val();
+  parsedFormData['name'] = nameFromForm ? nameFromForm : undefined;
+
+  const emailFromForm = $(`#form input[name="email"]`).val();
+  parsedFormData['email'] = emailFromForm ? emailFromForm : undefined;
+
+  const tripFromForm = $(`#form input[name="tripID"]`).val();
+  parsedFormData['tripID'] = tripFromForm ? tripFromForm : undefined;
+
+  return parsedFormData;
+};
+
+const clearForm = () => {
+  $(`form input[name="name"]`).val('');
+  $(`form input[name="email"]`).val('');
+  $(`form input[name="trip"]`).val('');
+}
+
+const createReservation = (event) => {
+  reportStatus("Submitting new reservation....");
+  event.preventDefault();
+
+  const tripData = readFormData();
+  console.log(tripData);
+  const postURL = `https://trektravel.herokuapp.com/trips/${tripData.tripID}/reservations`
+  axios.post(postURL, tripData)
+  .then((response) =>{
+    console.log(response);
+    reportStatus('Sucessfully added a reservation!!!');
+    clearForm();
+  })
+  .catch((error) =>{
+    console.log(error.response);
+    if (error.response.data && error.response.data.errors) {
+      reportError(
+        `Encountered an error: ${error.message}`,
+        error.response.data.errors
+      );
+    } else {
+      reportStatus(`Encountered an error: ${error.message}`);
+    }
+  });
+}
 
 const showTrip = (id) => {
 const singleURL = `https://trektravel.herokuapp.com/trips/${id}`;
@@ -52,8 +112,11 @@ const singleURL = `https://trektravel.herokuapp.com/trips/${id}`;
     }    })
 };
 
-const bookForm = () =>{
+const bookForm = (id) =>{
   $('#form').show();
+  document.getElementById('tripID').value = id
+
+
 
 }
 
@@ -63,5 +126,6 @@ $(document).ready(() => {
     showTrip(this.id);
     console.log(this.id);
     bookForm(this.id)
-  })
+  });
+  $('#form').submit(createReservation);
 });
