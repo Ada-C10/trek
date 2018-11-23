929ea92fc8316e8f01f842dc507a736cc38a3181
Sigrid Benezra <sdbenezra@gmail.com>
2018-11-23 12:34:27 -0800

    Add reservation form.

--- trek.js
+++ trek.js
@@ -28,6 +28,8 @@ const listTrips = () => {
 const showTripDetail = (id) => {
   const tripDetail = $('#trip-detail');
   tripDetail.empty();
+  const tripReservation = $('#reservation-form');
+  tripReservation.empty();
 
   axios.get( `https://trektravel.herokuapp.com/trips/${id}`)
 
@@ -40,27 +42,34 @@ const showTripDetail = (id) => {
     tripDetail.append(`<p class="list-group-item"><strong>Weeks:</strong>&nbsp${response.data.weeks}</p>`);
     tripDetail.append(`<p class="list-group-item"><strong>Cost:</strong>&nbsp$${parseFloat(response.data.cost).toFixed(2)}</p>`);
     tripDetail.append(`<p class="list-group-item"><strong>About:</strong><br>${response.data.about}</p>`);
+
+    tripReservation.append('<h2 class="card-title list-group-item">Reserve Trip</h2>')
+    console.log(`${response.data.name}`);
+    tripReservation.append(
+      `<form>
+        <div class="card-body">
+          <div class="card-text">
+            <label for="cust-name">Your Name:</label>
+            <input type="text" name="cust-name"/>
+          </div>
+          <div class="card-text">
+            <label for="email">Email:</lable>
+            <input type="text" name="email"/>
+          </div>
+          <div class="card-text">
+            <label for="trip-name">Trip Name:</label>
+            <input type="text name="trip-name" value="${response.data.name}"
+          </div>
+          <div class="card-text">
+            <input type="submit" name="add-reservation" value="Add Reservation" id="create-reservation" />
+        </div>
+      </form>`)
   })
   .catch((error) => {
     reportStatus(error);
     console.log(error);
   });
 };
-
-
-// const buildIncrementer = (name) => {
-//   let callCount = 0;
-//
-//   const callCountingFunction = () => {
-//     callCount += 1;
-//     console.log(`This is call number ${callCount} for function ${name}`);
-//   }
-//
-//   return callCountingFunction;
-// };
-
-
-
 
 $(document).ready(() => {
   $("#view-trip").click(listTrips);
