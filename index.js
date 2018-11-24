9f966bd9ab1a4f2dcb6ec36c18b5b45d4183f7a3
Addison Cole <addison.cole85@gmail.com>
2018-11-24 13:08:25 -0800

    add function to create closure for each trip, which creates click function to return a trips details.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://trektravel.herokuapp.com/trips';
+const BASE_URL = 'https://trektravel.herokuapp.com/trips/';
 
 //
 // Status Management
@@ -18,26 +18,68 @@ const reportError = (message, errors) => {
   reportStatus(content);
 };
 
-const loadPets = () => {
-  reportStatus('Loading pets...');
+const loadTrips = () => {
+  reportStatus('Loading inventory...');
 
   // Prep work
-  const petList = $('#pet-list');
-  petList.empty();
+  const tripsList = $('#trips-list');
+  tripsList.empty();
 
-  // Actually load the pets
+  // Actually load the trips
   axios.get(BASE_URL)
     .then((response) => {
-      reportStatus(`Successfully loaded ${response.data.length} pets`);
-      response.data.forEach((pet) => {
-        petList.append(`<li>${pet.name}</li>`);
+      reportStatus(`Successfully loaded ${response.data.length} trips`);
+      response.data.forEach((trip) => {
+        const newTrip = $(`<li class="trip ">${trip.name}</li>`);
+        tripsList.append(newTrip);
+        const showTrip = tripShowHandler(trip);
+        newTrip.click(showTrip);
       });
     })
     .catch((error) => {
-      reportStatus(`Encountered an error while loading pets: ${error.message}`);
+      reportStatus(`Encountered an error while loading trips: ${error.message}`);
       console.log(error);
     });
 };
+
+const tripShowHandler = (trip) => {
+
+  // Prep work
+  const tripsShow = $('#trips-list');
+  const tripID = trip.id;
+  return () => {
+    reportStatus('Loading trip...');
+    tripsShow.empty();
+  // Actually load the trips
+  axios.get(BASE_URL + tripID)
+    .then((response) => {
+      reportStatus(`Found your get-away`);
+        const newTrip = $(`<li>${response.data.name}, $${response.data.cost}</li><li>${response.data.continent}, ${response.data.weeks} weeks</li><li>Trip type:${response.data.category}</li><li>Details:${response.data.about}</li></ul>`);
+        tripsShow.append(newTrip);
+        const reserveButton = $(`<button class="reserve">Sign me up!</button>`)
+        tripsShow.append(reserveButton);
+      })
+
+    .catch((error) => {
+      reportStatus(`Encountered an error while loading trips: ${error.message}`);
+      console.log(error);
+    });
+  };
+};
+
+// const tripReserveHandler = (trip) => {
+//
+//   // Prep work
+//   const tripsShow = $('#trips-list');
+//   // const tripID = trip.id;
+//   return () => {
+//     reportStatus('Loading resevation...');
+//     tripsShow.empty();
+//   tripsShow.append('Reservation here');
+//     }
+//
+// };
+
 //
 // OK GO!!!!!
 //
