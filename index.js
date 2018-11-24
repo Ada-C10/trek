1672728675b5dc2d26b25302f461a9e204d428b1
Valerie Gidzinski <valgidzi@gmail.com>
2018-11-24 04:08:42 -0800

    Refactor get request methods

--- index.js
+++ index.js
@@ -4,53 +4,51 @@ const reportStatus = (message) => {
   $('#status-message').html(message);
 };
 
-const getAllTrips = () => {
-  reportStatus("Loading trips...")
-  const tripList = $('#trip-list');
-  tripList.empty();
-
-  axios.get(URL)
-    .then((response) => {
-      reportStatus(`Successfully loaded ${response.data.length} trips.`);
-      response.data.forEach((trip) => {
-        tripList.append(`<li><button id="${trip.id}">${trip.name}</button></li>`);
+const getRequestHandler = (id) => {
+  const sendGetRequest = () => {
+    reportStatus('Loading...')
+    let url = URL + id;
+    axios.get(url)
+      .then((response) => {
+        reportStatus('Successfully loaded!')
+        handleGetResponse(response);
+      })
+      .catch((error) => {
+        reportStatus(`Encountered an error ${error.message}`);
       });
-    })
-    .catch((error) => {
-      reportStatus(`Encountered an error ${error.message}`);
-    });
+  };
+  return sendGetRequest;
 };
 
-const getTripDetail = (id) => {
-  reportStatus("Loading trip...")
-  const tripDetailList = $('#trip-detail-list');
-  tripDetailList.empty();
-
-  axios.get(URL)
-    .then((response) => {
-      return axios.get(URL + '/' + id);
-    })
-    .then((response) => {
-      reportStatus("Successfully loaded trip!")
-      let trip = response.data
-      tripDetailList.append(
-        `<li>Name: ${trip.name}</li>
-        <li>Continent: ${trip.continent}</li>
-        <li>Category: ${trip.category}</li>
-        <li>Weeks: ${trip.weeks}</li>
-        <li>Cost: ${trip.cost}</li>
-        <li>About: ${trip.about}</li>`
-      );
-    })
-    .catch((error) => {
-      reportStatus(`Encountered an error ${error.message}`);
+const handleGetResponse = (response) => {
+  if (response.data.length > 1) {
+    let tripList = $('#trip-list');
+    tripList.empty();
+    response.data.forEach((trip) => {
+      tripList.append(`<li><button id="${trip.id}">${trip.name}</button></li>`);
     });
+  } else {
+    let tripList = $('#trip-detail-list');
+    tripList.empty();
+    let trip = response.data;
+    tripList.append(
+      `<li>Name: ${trip.name}</li>
+      <li>Continent: ${trip.continent}</li>
+      <li>Category: ${trip.category}</li>
+      <li>Weeks: ${trip.weeks}</li>
+      <li>Cost: ${trip.cost}</li>
+      <li>About: ${trip.about}</li>`
+    );
+  }
 };
 
 $(document).ready(() => {
+  const getAllTrips = getRequestHandler('/');
   $('#load-all-trips').click(getAllTrips);
+
   $('ul').on('click', 'button', function(event) {
-    let id = $(this).attr('id');
-    getTripDetail(id);
+    let id = '/' + $(this).attr('id');
+    const getTripDetails = getRequestHandler(id);
+    getTripDetails();
   });
 });
