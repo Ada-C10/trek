e4aaa50488dc04ec2563e0d6564d29b40fbb2039
Semret Nicodimos <snicodimos@gmail.com>
2018-11-24 12:04:27 -0800

    added a function that loads one trip details

--- index.js
+++ index.js
@@ -5,26 +5,56 @@ const reportStatus = (message) => {
   $('#status-message').html(message);
 };
 
+
+//Loading all trips
 const loadTrips = () => {
-  const tripList = $('#travel-list');
+  const tripList = $('#trip-list');
   tripList.empty();
 
   axios.get(BaseURL)
-  .then((response) =>{
+  .then((response) => {
     response.data.forEach((trip) => {
-      tripList.append(`<li>${trip.name}</li>`)
+      let single_trip = $(`<li>${trip.name}</li>`)
+      tripList.append(single_trip);
+
+      // call the load one trip function to pass in the trip info so that at a click it will show
+      const loadtrip = loadOneTrip(trip);
+      single_trip.click(loadtrip);
     });
   })
+
   .catch((error) => {
-    reportStatus(`Encountered an error while loading pets: ${error.message}`);
+    reportStatus(`Encountered an error while loading trips: ${error.message}`);
   });
-
-
-
 };
 
+const loadOneTrip = (trip) => {
+
+  return () => {
+    const oneTrip = $('#one-trip');
+    oneTrip.empty();
+
+    axios.get(BaseURL + '/'+ trip.id)
+    .then((response) => {
+
+      oneTrip.append(`<h3>Trip Name: ${response.data.name}</h3>
+        <p>ID: ${response.data.id}</p>
+        <p>Continent: ${response.data.continent}</p>
+        <p>About: ${response.data.about}</p>
+        <p>Category: ${response.data.category}</p>
+        <p>Duration: ${response.data.weeks}</p>
+        <p>Cost: ${response.data.cost}</p>`);
+      });
+    };
+  };
 
 
-$(document).ready(()=> {
-  $('#load').click(loadTrips)
-});
+  const reserveTrip = () => {
+
+  };
+
+
+  $(document).ready(()=> {
+    $('#load-all').click(loadTrips)
+
+  });
