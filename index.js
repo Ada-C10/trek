ec517e9796b9a14b5fc55340e0fdc4d7f3a4c14f
Ada <larmartinez2@gmail.com>
2018-11-24 23:11:35 -0800

    refactor code and added some css

--- index.js
+++ index.js
@@ -31,10 +31,12 @@ const loadTrips = () => {
     .then((response) => {
       reportStatus(`Successfully loaded ${response.data.length} trips`);
       response.data.forEach((trip) => {
-        const div = $(`<div class="trip-${trip.id}"><button type="button">${trip.name}</button></div>`) // make the div have the class not the button, so that eveything is added to this div, make sure that you specify the type of button
+        const div = $(`<div class="trip-${trip.id}"><button >${trip.name}</button></div>`) // make the div have the class not the button, so that eveything is added to this div, make sure that you specify the type of button
         tripList.append(div);
         $('button', div).click(() => { // to only activate on button inside the div
           loadTrip(`.trip-${trip.id}`);
+          createForm(`.trip-${trip.id}`);
+          $(`.trip-${trip.id}`).on('submit', '#trip-form', createReservation);
         })
       });
 
@@ -51,7 +53,6 @@ const loadTrip = (tripinfo) => {
   num = num.join("");
   const trip = $(`${tripinfo}`);
   trip.empty();
-  trip.append(`<h2> Trip details </h2>`)
   axios.get(URL + "/" + num)
     .then((response) => {
         let data = {}
@@ -60,25 +61,28 @@ const loadTrip = (tripinfo) => {
           data["Continent"] = response.data.continent
           data["Details"] = response.data.about
           data["Category"] = response.data.category
-          data["Duration"] = response.data.weeks
-          data["Cost"] = response.data.cost
+          data["Weeks"] = response.data.weeks
+          data["Cost"] = '$' + response.data.cost
           const list = $('<ul style="list-style-type:none"></ul>')
           Object.keys(data).forEach(function(key) {
               list.append(`<li><strong>${key}</strong> : ${data[key]}</li`);
           });
-        trip.append(list)
+        const section = $('<section></section>');
+        section.append('<h2> Trip details </h2>')
+        section.append(list)
+        trip.append(section)
     })
     .catch((error) => {
       reportStatus(`Encountered an error while loading trip: ${error.message}`);
     });
-    createForm(tripinfo);
-    $(tripinfo).on('submit', '#trip-form', createReservation);
+    //createForm(tripinfo);
+    //$(tripinfo).on('submit', '#trip-form', createReservation);
 
 };
 
 const createForm = (tripinfo) => {
 
-  event.preventDefault(); // to prevent it from reloading, but still doesn't work
+  event.preventDefault();
 
   // get id number
   let num = tripinfo.match(/\d/);
@@ -89,7 +93,7 @@ const createForm = (tripinfo) => {
   //generate form
   // Create a section element (not in the DOM)
   const section = $('<section></section>');
-  section.append('<h1>Reserve Trip</h1>');
+  section.append('<h2>Reserve Trip</h2>');
 
   const form = $('<form id = "trip-form"></form>')
   const divName = $('<div></div>')
@@ -103,6 +107,7 @@ const createForm = (tripinfo) => {
 
   form.append(divName)
   form.append(divEmail)
+
   form.append(`<input type="hidden" id="tripId" name="triptId" value=${num}>`)
   form.append('<input type="submit" name="add-pet" value="Reserve" />')
 
@@ -155,8 +160,6 @@ const createReservation = (event) => {
     });
 };
 
-//
-// OK GO!!!!!
 // this is the homepage! the trip will not be avaiable
 $(document).ready(() => {
   $('#load').click(loadTrips);
