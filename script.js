cd8ac6096e7c143dcd4a6f85c1fb087d43ede003
kangazoom <michellelcronin@gmail.com>
2018-11-24 16:29:45 -0800

    can load trips; very basic

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 // move to its own folder?
+const baseURL = 'https://trektravel.herokuapp.com/';
+const getTripsURL = 'trips';
+
+const reportStatus = (message) => {
+  $('.status-message').html(message);
+}
+
+const loadTrips = () => {
+  reportStatus('Loading trips...')
+
+// wrap up into one function?
+const tripList = $('.trip-list');
+tripList.empty();
+//
+
+axios.get(baseURL+getTripsURL)
+  .then((response) => {
+    // reportStatus(`Successfully loaded ${response.data.length} trips.`)
+    $(tripList).append(`<p>${response.data.length}</p>`);
+
+    response.data.forEach((trip) => {
+    $(tripList).append(`<p>${trip.name}</p>`);
+  });
+  });
+
+
+  // .catch((error) => {
+  //   reportStatus(`Encountered an error while loading trips: ${error.message}`);
+  // });
+};
+
+$(document).ready(() => {
+  $('body').hover(loadTrips);
+
+});
