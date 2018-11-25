8e9d3aa82f465cdf4253c8a74d08aa8e479963d2
Pushpa Agr <pushpaagr108@gmail.com>
2018-11-25 12:30:20 -0800

    added logic to have form be displayed when user clicks on a trip, and forms accurately sends data back to make a successful reservation

--- index.js
+++ index.js
@@ -1,73 +1,48 @@
 //wave 1
 const URL = 'https://trektravel.herokuapp.com/trips';
+let currentTripID = 0;
 
 const reportStatus = (message) => {
   $('#status-message').html(message);
 };
 
+const reportError = (message, errors) => {
+  let content = `<p>${message}</p><ul>`;
+  for (const field in errors) {
+    for (const problem of errors[field]) {
+      content += `<li>${field}: ${problem}</li>`;
+    }
+  }
+  content += "</ul>";
+  reportStatus(content);
+};
+
+//load trips wave 1
 const loadTrips = () => {
-  reportStatus("loading trip detail...")
+  reportStatus("loading trip detail...");
 
   const tripList = $('#trip-list');
   tripList.empty();
 
   axios.get(URL)
   .then((response) => {
-    reportStatus(`successfully loaded ${response.data.length}`)
+    reportStatus(`successfully loaded ${response.data.length} trips`);
     response.data.forEach((trip) => {
       const trips = $(`<li>${trip.name}</li>`)
       tripList.append(trips);
-
       trips.on('click', () => {
         loadTripDetails(`${trip.id}`)
-      })
-
+      });
       trips.on("click", function() {
         $("#addForm").toggle()
-        loadReservationForm(`${trip.id}`)
-
+        currentTripID = trip.id
       });
-
-
     });
   })
   .catch((error) => {
     reportStatus(error);
   });
-
 };
-
-// wave 3
-
-// const loadReservationForm = (tripId) => {
-//   const URLPOST = `https://trektravel.herokuapp.com/trips/${tripId}/reservations`
-// }
-//
-// const CreateReservation = () => {
-//
-//   event.preventDefault();
-//
-//   console.log("in CreateReservation");
-//   reportStatus('Sending trip data....');
-//
-//
-//   const dataToSendWithPost = {
-//     name: $('input[name="name"]').val(),
-//     age:  $('input[name="email"]').val()
-//
-//     axios.post(URLPOST, dataToSendWithPost)
-//     .then((response) => {
-//       reportStatus(`Successfully added a pet with ID ${response.data.id}! and the name ${response.data.name}, age: ${response.data.age}`);
-//     })
-//     .catch((error) => {
-//       reportStatus(error);
-//       console.log(error);
-//     });
-//   }
-//
-// }
-
-
 
 //wave 2
 
@@ -82,9 +57,8 @@ const loadTripDetails = (tripId) => {
 
   axios.get(URL1+`${tripId}`)
   .then((response) => {
-    reportStatus(`successfully loaded ${response.data.length}`)
+    reportStatus(`successfully loaded individual trip ${response.data.id}`)
     tripDetail.append(`<p> Trip Detail </p>`);
-
     tripDetail.append(`<li>Id: ${response.data.id}</li>`);
     tripDetail.append(`<li>Name: ${response.data.name}</li>`);
     tripDetail.append(`<li>continent: ${response.data.continent}</li>`);
@@ -100,8 +74,56 @@ const loadTripDetails = (tripId) => {
 };
 
 
+// wave 3 -creating a reservation
+
+const readFormData = () => {
+  const parsedFormData = {};
+
+  const nameFromForm = $(`#addForm input[name="name"]`).val();
+  parsedFormData['name'] = nameFromForm ? nameFromForm : undefined;
+
+  const emailFromForm = $(`#addForm input[name="email"]`).val();
+  parsedFormData['email'] = emailFromForm ? emailFromForm : undefined;
+
+  return parsedFormData;
+};
+
+const clearForm = () => {
+  $(`#addForm input[name="name"]`).val('');
+  $(`#addForm input[name="email"]`).val('');
+}
+
+const createReservation = (event) => {
+  // Note that createPet is a handler for a `submit`
+  // event, which means we need to call `preventDefault`
+  // to avoid a page reload
+  event.preventDefault();
+
+  const reservationData = readFormData();
+  console.log(reservationData);
+
+  reportStatus('Sending reservation data...');
+
+  axios.post((URL + `/${currentTripID}/reservations`), reservationData)
+  .then((response) => {
+    reportStatus(`Successfully added a reservation with ID ${response.data.id}!`);
+    clearForm();
+  })
+  .catch((error) => {
+    console.log(error.response);
+    if (error.response.data && error.response.data.errors) {
+      reportError(
+        `Encountered an error: ${error.message}`,
+        error.response.data.errors
+      );
+    } else {
+      reportStatus(`Encountered an error: ${error.message}`);
+    }
+  });
+};
+
 
 $(document).ready(() => {
-  $('#load').click(loadTrips);
-  $('#addForm').submit(CreateReservation);
+  $('#button-load-trips').click(loadTrips);
+  $('#addForm').submit(createReservation);
 });
