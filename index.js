c7d258987a37d951d53fe31fd8aa93d25d8b1e28
Kat Perreira <kat.perreira@gmail.com>
2018-11-25 18:36:58 -0800

    wave 3 finished reservations

--- index.js
+++ index.js
@@ -14,20 +14,31 @@ const reportError = (message, errors) => {
   }
 }
 
-const loadTrips = () => {;
-  statusReport("Trips Loading...");
+const generateForm = (section) => {
+  section.empty();
+  section.append('<h1> Reserve a Trip </h1>');
 
+  section.append('<form id="trip-form">');
+
+  const form = $('#trip-form');
+
+  form.append('<div><label for="name">Name</label><input type="text" name="name"/></div>');
+  form.append('<div><label for="age">Age</label><input type="number" name="age"/></div>');
+  form.append('<div><label for="email">Email</label><input type="text" name="email"/></div>');
+  form.append('<div><input type="submit" name="add-trip" value="Reserve" /></div>');
+
+};
+
+
+const loadTrips = () => {
+  statusReport("Trips Loading...");
   // this bit keeps stuff from getting added on more and more each time stuff appends
   let tripList = $('#trip-list');
   tripList.empty();
-
   axios.get(URL)
-
   // promise
-
   .then((response) => {
     statusReport (`Successfully loaded ${response.data.length} trip`);
-
     response.data.forEach((trip) => {
       let name = `<li>${trip.name} </li>`
       tripList.append(name);
@@ -40,7 +51,7 @@ const loadTrips = () => {;
           // promise
           .then((response) => {
             statusReport('Successfully loaded ${trip.name}');
-
+            // connect trip details to the id in html
             const tripDetails = $('#trip-details');
             // empty it out before appending
             tripDetails.empty();
@@ -51,14 +62,52 @@ const loadTrips = () => {;
             tripDetails.append(`<h3> Weeks: ${response.data.weeks}</h3>`);
             tripDetails.append(`<h3> Cost: ${response.data.cost}</h3>`);
             tripDetails.append(`<h3> About: ${response.data.about}</h3>`);
+            // start reserve a trip form
+            const form = $('#reserve');
+            generateForm(form);
+          })
+          .catch((error) => {
+            statusReport(error);
+          });
+        }
+      };
+      // make a post request to reserve a trip
+      const reservation = (trip) => {
+        return (reservation) => {
+          reservation.preventDefault();
+
+          const data = {
+            'name': $('input[name="name"]').val(),
+            'age': $('input[name="age"]').val(),
+            'email': $('input[name="email"]').val(),
+          }
+
+          axios.post(URL + `/${trip.id}/reservations`, data)
+          // promise
+
+          .then((response) => {
+            statusReport(`Successfully added trip reservation with name: ${response.data.name}`);
+          })
+          // if not, error
+
+          .catch((error) => {
+            if (error.response.data && error.response.data.errors) {
+              reportError(
+                `Encountered an error: ${error.message}`,
+                error.response.data.errors
+              );
+            } else {
+              statusReport(`Encountered an error: ${error.message}`);
+            }
           })
         }
       }
-
-
-       const showIndTrip = showTrip(trip);
-       $('li:last').click(showIndTrip);
-     })
+      const showIndTrip = showTrip(trip);
+      $('li:last').click(showIndTrip);
+      const makeReservation = reservation(trip);
+      // use .on not .click to submit the thing
+      $('#reserve').on('submit', '#trip-form', makeReservation);
+    })
   })
 
   // catch the errors if promise isnt kept
@@ -68,11 +117,7 @@ const loadTrips = () => {;
   });
 };
 
-
-
-
+// trigger the event for clicking, starting the whole thing
 $(document).ready(() => {
   $('#load').click(loadTrips);
-  // step 1.
-
 });
