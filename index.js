192ffb2a3cfc6f22ed64fa23863b1e9f7d5aee73
Katricia Smith <smithkatricia@gmail.com>
2018-11-25 22:23:56 -0800

    Delete bootstrap link ref to fix css. Add CSS for list and detail sections

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ const loadTrips = () => {
   axios.get(URL)
     .then((response) => {
       reportStatus(`Successfully loaded ${response.data.length} trips`);
+      tripList.append('<h3>All Trips</h3>')
+
       response.data.forEach((trip) => {
         // tripList.append(`<a href="${URL}/${trip.id}" id="trip-button"><li class ="trip-details">${trip.name}</li></a>`);
         tripList.append(`<li class ="trip-details"><a class="trip-button" id=${trip.id}>${trip.name}</a></li>`);
@@ -55,6 +57,8 @@ const loadDetails = (id) => {
   axios.get(URL + id)
   .then((response) => {
     // $('#trip-blurb').append(`<p>${response.data.find(x => x.id ==$(this.id)).about}</p>`);
+    tripBlurb.append('<h3>Trip Details</h3>');
+
     for (let tripInfo in response.data) {
       tripBlurb.append(`<li><strong>${tripInfo}</strong>: ${response.data[tripInfo]} </li>`);
   }
