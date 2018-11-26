b049b07bbd26a47a0f4c26596310abc26faeef70
Karis Kim <ouniiq@hotmail.com>
2018-11-26 15:35:27 -0800

    worked with Charles on how to make my version of the code to pass. the previous version is the code that I submitted after getting help from Addy, yay Addy. It seems I just needed to add event inside line 48 to make this work

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const loadAllTreks = () => {
   const trekList = $('#trek-list');
   trekList.empty();
 
+  // Actually load the pets
   axios.get(treksUrl)
     .then((response) => {
       response.data.forEach((trek) => {
@@ -17,9 +18,37 @@ const loadAllTreks = () => {
 
         trekList.append(listItem);
         listItem.on('click', () => {
-          $('.new-trek').empty();
           loadOneTrek(`${trek.id}`);
-          reserveTrek(`${trek.id}`);
+          $('.new-trek').empty();
+          $('.new-trek').append(`<h1 id="testing">Reserve trek</h1>
+          <form id="trek-form">
+            <div>
+              <label for="name">Trek name</label>
+              <input type="text" name="name"/>
+            </div>
+
+            <div>
+              <label for="age">Age</label>
+              <input type="text" name="age"/>
+            </div>
+
+            <div>
+              <label for="email">Email</label>
+              <input type="text" name="email"/>
+            </div>
+
+            <input type="submit" name="add-trek" value="Add Trek" />
+          </form>
+
+          <div class="">
+            <button id="load-detail">Details</button>
+            <ul id="trek-detail"></ul>
+          </div>`);
+
+          $('#trek-form').on('submit', (event) => {
+              event.preventDefault();
+             reserveTrek(`${trek.id}`);}
+            );
         });
       });
     })
@@ -34,18 +63,15 @@ const loadOneTrek = (id) => {
 
   axios.get(trekNumber)
     .then((response) => {
-      $('.new-trek').append(`<div class="">
-        <button id="load-detail">Details</button>
-        <ul id="trek-detail"></ul>
-      </div>`);
+      $('#trek-detail').empty();
       const trekData = response.data
-        $('#trek-detail').append(`<li> Id: ${trekData.id}</li>`);
-        $('#trek-detail').append(`<li> Name:  ${trekData.name}</li>`);
-        $('#trek-detail').append(`<li> Continent:  ${trekData.continent}</li>`);
-        $('#trek-detail').append(`<li> Category:  ${trekData.category}</li>`);
-        $('#trek-detail').append(`<li> Weeks:  ${trekData.weeks}</li>`);
-        $('#trek-detail').append(`<li> Cost: $ ${trekData.cost}</li>`);
-        $('#trek-detail').append(`<li> About:  ${trekData.about}</li>`);
+        $('#trek-detail').append(`<li> ${trekData.id}</li>`);
+        $('#trek-detail').append(`<li> ${trekData.name}</li>`);
+        $('#trek-detail').append(`<li> ${trekData.continent}</li>`);
+        $('#trek-detail').append(`<li> ${trekData.about}</li>`);
+        $('#trek-detail').append(`<li> ${trekData.category}</li>`);
+        $('#trek-detail').append(`<li> ${trekData.weeks}</li>`);
+        $('#trek-detail').append(`<li> ${trekData.cost}</li>`);
     })
     .catch((error) => {
       $('#status-message').append(`${error}`);
@@ -60,37 +86,6 @@ const clearForm = () => {
 
 const reserveTrek = (id) => {
   // id.preventDefault();
-  const formHtml = `<h1 id="testing">Reserve trek</h1>
-  <form id="trek-form">
-    <div>
-      <label for="name">Trek name</label>
-      <input type="text" name="name"/>
-    </div>
-
-    <div>
-      <label for="age">Age</label>
-      <input type="text" name="age"/>
-    </div>
-
-    <div>
-      <label for="email">Email</label>
-      <input type="text" name="email"/>
-    </div>
-
-    <input type="submit" name="add-trek" value="Add Trek" />
-  </form>`;
-
-    $('.new-trek').append(formHtml);
-
-    const result = (event) => {
-      event.preventDefault();
-      createReservation( id );
-    };
-
-    $('#trek-form').submit(result);
-};
-
-const createReservation = (id) => {
   const reserveTrekUrl = 'https://trektravel.herokuapp.com/trips/' + id + '/reservations';
 
   const trekData = {
@@ -98,20 +93,19 @@ const createReservation = (id) => {
       age: $('#trek-form input[name="age"]').val(),
       email: $('#trek-form input[name="email"]').val()
     };
-
   reportStatus('Sending trek data...');
-
   axios.post(reserveTrekUrl, trekData)
     .then((response) => {
       console.log(response);
-      reportStatus('Successfully reserved trip');
       $('#testing').append(response);
       clearForm();
     })
     .catch((error) => {
       $('#status-message').append(`${error}`);
     });
-}
+};
+
+
 
 $(document).ready(() => {
   $('#load').click(loadAllTreks);
