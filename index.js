47ce42a2c1bf0a90a9dc3f25430f96090ed3d266
Semret Nicodimos <snicodimos@gmail.com>
2018-11-25 21:45:51 -0800

    implemented success and error messages

--- index.js
+++ index.js
@@ -5,14 +5,30 @@ const reportStatus = (message) => {
   $('#status-message').html(message);
 };
 
+const reportError = (message, errors) => {
+  let content = `<p>${message}</p>`
+  content += "<ul>";
+  for (const field in errors) {
+    for (const problem of errors[field]) {
+      content += `<li>${field}: ${problem}</li>`;
+    }
+  }
+  content += "</ul>";
+  reportStatus(content);
+}
+
 
 //Loading all trips
 const loadTrips = () => {
+  reportStatus("loading awesome trips...");
+
+
   const tripList = $('#trip-list');
   tripList.empty();
 
   axios.get(BaseURL)
   .then((response) => {
+    reportStatus(`Successfully loaded  ${response.data.length} adventurous trips`);
 
     title = $(`<h3><strong>List of trips</strong></h3>`);
     tripList.append(title);
@@ -43,6 +59,8 @@ const loadOneTrip = (id) => {
     axios.get(BaseURL + '/'+ id)
     .then((response) => {
 
+      reportStatus(`Successfully loaded details for  ${response.data.name} trip`);
+
       oneTrip.append(`<section class="info"><h4>Here is the details for <p>
         <strong> ${response.data.name}</strong></p></h4>
         <p><strong>ID:</strong> ${response.data.id}</p>
@@ -51,37 +69,41 @@ const loadOneTrip = (id) => {
         <p><strong>Category:</strong> ${response.data.category}</p>
         <p><strong>Duration:</strong> ${response.data.weeks} week(s)</p>
         <p><strong>Cost:</strong> $${response.data.cost}</p></section>`);
+  })
+  .catch((error) => {
+    reportStatus(`Encountered an error while loading the trip: ${error.message}`);
   });
 };
 
 // function to display a form to get info to reserve trip
 const generateForm = (trip) => {
   const form = $('#displayForm');
-    form.empty();
+  form.empty();
 
-    form.append(`
-      <form id="trip-form">
-      <h4>Reserve your spot for <strong>${trip.name} trip </strong> now by completing this form</h4>
-      <p> </p>
-      <div class="form-group">
-        <label for="name">Your Name</label>
-        <input type="text" class="form-control" name="name" placeholder="Full Name"/>
-      </div>
+  form.append(`
+    <form id="trip-form">
+    <h4>Reserve your spot for <strong>${trip.name} trip </strong> now by completing this form</h4>
+    <p> </p>
+    <div class="form-group">
+    <label for="name">Your Name</label>
+    <input type="text" class="form-control" name="name" placeholder="Full Name"/>
+    </div>
 
-      <div class="form-group">
-        <label for="email">Email Address</label>
-        <input type="text" class="form-control" name="email" placeholder="Email Address" />
-      </div>
+    <div class="form-group">
+    <label for="email">Email Address</label>
+    <input type="text" class="form-control" name="email" placeholder="Email Address" />
+    </div>
 
-      <input type="submit" name="reserve" value="Reserve" type="button" class="btn btn-primary btn-lg"/>
-      </form>`);
+    <input type="submit" name="reserve" value="Reserve" type="button" class="btn btn-primary btn-lg"/>
+    </form>`);
 
-      const reserve = (event) => {
-        event.preventDefault();
-        createReservation(trip.id);
-      };
-      $('#trip-form').submit(reserve);
-};
+    const reserve = (event) => {
+      event.preventDefault();
+      reportStatus('Sending reservation data...');
+      createReservation(trip.id);
+    };
+    $('#trip-form').submit(reserve);
+  };
 
 // function to clear the form ones reserve button is pressed
 const clearForm = () => {
@@ -105,21 +127,26 @@ const readFormData = () => {
 // function to send a post request to reserve the trip
 const createReservation = (id) => {
 
-    const reserveData = readFormData();
+  const reserveData = readFormData();
 
-    const resURL = BaseURL + "/" + id + "/reservations";
+  const resURL = BaseURL + "/" + id + "/reservations";
 
-    axios.post(resURL, reserveData)
-      .then((response) => {
-        console.log(response);
-          clearForm();
+  axios.post(resURL, reserveData)
+  .then((response) => {
+    reportStatus('Successfully reserved your trip request. We will shortly send you a confirmation via email.');
+    clearForm();
+  })
 
-      })
-
-      .catch((error) => {
-
-      });
-
+  .catch((error) => {
+    if (error.response.data && error.response.data.errors) {
+      reportError(
+        `Encountered an error: ${error.message}`,
+        error.response.data.errors
+      );
+    } else {
+      reportStatus(`Encountered an error: ${error.message}`);
+    }
+  });
 };
 
 
