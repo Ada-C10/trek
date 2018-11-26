69bbc5e980d0a551756160d490982178921ed2dc
Semret Nicodimos <snicodimos@gmail.com>
2018-11-25 20:02:57 -0800

    added a background image with some padding for list of trips

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ const loadTrips = () => {
   axios.get(BaseURL)
   .then((response) => {
 
-    title = $(`<h1>List of trips</h1>`);
+    title = $(`<h3>List of trips</h3>`);
     tripList.append(title);
 
     response.data.forEach((trip) => {
-      const singleTrip = $(`<li class="${trip.id}">${trip.name}</li>`)
+      const singleTrip = $(`<li class="${trip.id}" class="list-group-item">${trip.name}</li>`)
       tripList.append(singleTrip);
 
       // call the load one trip function to pass in the trip id so that at a click it will show the trip detail and the form to reserve it
@@ -43,14 +43,14 @@ const loadOneTrip = (id) => {
     axios.get(BaseURL + '/'+ id)
     .then((response) => {
 
-      oneTrip.append(`<h2>Trip Details</h2>
+      oneTrip.append(`<h3>Trip Details</h3>
         <h4><strong>Name: </strong>${response.data.name}</h4>
         <p><strong>ID:</strong> ${response.data.id}</p>
         <p><strong>Continent:</strong> ${response.data.continent}</p>
         <p><strong>About:</strong> ${response.data.about}</p>
         <p><strong>Category:</strong> ${response.data.category}</p>
-        <p><strong>Duration:</strong> ${response.data.weeks}</p>
-        <p><strong>Cost:</strong> ${response.data.cost}</p>`);
+        <p><strong>Duration:</strong> ${response.data.weeks} week(s)</p>
+        <p><strong>Cost:</strong> $${response.data.cost}</p>`);
   });
 };
 
