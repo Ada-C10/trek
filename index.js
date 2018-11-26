238c5d28348d01fcee6c36572b50c3932424749e
Katricia Smith <smithkatricia@gmail.com>
2018-11-25 20:45:02 -0800

    Comment out second const init and append. Did not add info

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const loadTrips = () => {
 
   const tripList = $('#trip-list');
   tripList.empty();
+  // const infoList = $('#info-list');
 
   axios.get(URL)
     .then((response) => {
@@ -29,7 +30,7 @@ const loadTrips = () => {
       response.data.forEach((trip) => {
         // tripList.append(`<a href="${URL}/${trip.id}" id="trip-button"><li class ="trip-details">${trip.name}</li></a>`);
         tripList.append(`<li class ="trip-details"><a class="trip-button" id=${trip.id}>${trip.name}</a></li>`);
-        tripList.append(`<p>continent: ${trip.continent}, weeks: ${trip.weeks}, category: ${trip.category}</p>`);
+        tripList.append(`<li>continent: ${trip.continent}, weeks: ${trip.weeks}, category: ${trip.category}</li>`);
       });
     })
     .catch((error) => {
