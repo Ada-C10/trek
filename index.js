14222361a61bb3ef68c070d062af9ab4d675c600
Shelan <shelandebesai@gmail.com>
2018-11-27 10:43:43 -0800

    created a non-functioning form

--- index.js
+++ index.js
@@ -46,10 +46,10 @@ const tripDetails = (trip) => {
   tripDetailsList.empty();
 
 
-  axios.get(URL + '/' + trip.id)
+  axios.get(URL + '/' + trip.id) //gets a trip
     .then((response) => {
       const tripData = response.data;
-      console.log(trip);
+      console.log(trip)
       tripDetailsList.append(`<li>Name: ${tripData.name}</li>`);
       tripDetailsList.append(`<li>Category: ${tripData.category}</li>`);
       tripDetailsList.append(`<li>Continent: ${tripData.continent}</li>`);
@@ -58,6 +58,80 @@ const tripDetails = (trip) => {
       tripDetailsList.append(`<li>About: ${tripData.about}</li>`);
     })
   }
+const reservationForm = (trip) => {
+  const form =$('#form')
+  form.empty();
+
+  form.append(`
+    <h2>Reserve a Trip</h2>
+    <div>
+      <label for="name">name</label>
+      <input type="text" name="name" />
+    </div>
+
+    <div>
+      <label for="email">Email</label>
+      <input type="text" name="email" />
+    </div>
+
+    <div>
+      <input type="hidden" name="trip_id" value=${trip.id} />
+    </div>
+    <input type="submit" name="reserve" value="Reserve" />
+  `);
+}
+
+//reserving trip
+const readFormData = () => {
+  const parsedFormData = {};
+
+  const tripsForm = $(`#form input[name="trip_id"]`).val();
+  parsedFormData['trip_id'] = tripsForm ? tripsForm : undefined;
+
+  const formName = $(`#form input[name="name"]`).val();
+  parsedFormData['name'] = formName  ? formName  : undefined;
+
+  const formEmail = $(`#form input[name="email"]`).val();
+  parsedFormData['email'] = formEmail ? formEmail : undefined;
+
+  return parsedFormData;
+};
+
+const clearForm = () => {
+  $(`#form input[name="name"]`).val('');
+  $(`#form input[name="email"]`).val('');
+  $(`#form input[name="trip"]`).val('');
+}
+
+const reserveTrip = (event) => { //handler for a `submit`
+  console.log(event);
+  event.preventDefault(); //prevents page from reloading
+
+  const tripInfo = readFormData(); //values from the form
+  console.log(tripInfo);
+
+  axios.post(URL + '/' + tripInfo.trip_id + '/reservations', tripInfo)
+   .then((response) => {
+     reportStatus(`Successfully reserved ${response.data.id}!`);
+     clearForm();
+   })
+   .catch((error) => {
+      console.log(error.response);
+      if (error.response.data && error.response.data.errors) {
+        reportError(
+          `Encountered an error: ${error.message}`,
+          error.response.data.errors
+        );
+      } else {
+        reportStatus(`Encountered an error: ${error.message}`);
+      }
+    });
+};
 $(document).ready(() => {
   $('#load').click(loadTrips);
+
+  $('#trip-list').on('click', 'li', function(trip) {
+    reservationForm(trip);
+  });
+  $('#form').submit(reserveTrip);
 });
