d77b5ce52c2598367aca62f1a498643b971e0aa5
sjlee3157 <sjlee3157@gmail.com>
2018-11-27 10:29:17 -0800

    Trip Details pane is styled

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const reportStatus = (message) => {
   $('#status-message').html(message);
 };
 
-const loadTripTable = () => {
-  const tripConsole = $('#trip-console');
-  tripConsole.empty();
+const loadAllTrips = () => {
+  const allTrips = $('#all-trips');
+  allTrips.empty();
   reportStatus('<strong>Loading trips...</strong>');
-  tripConsole.append('<table id="trip-table"></table>')
+  allTrips.append('<table id="trip-table"></table>')
   const tripTable = $('#trip-table');
   axios.get(URL)
     .then((response) => {
       reportStatus(`Successfully loaded ${response.data.length} trips`);
       tripTable.append('<tr><th><h1>All Trips</h1></th></tr>');
       response.data.forEach((trip) => {
-        tripTable.append(`<tr><td>${trip.name}</td></tr>`);
+        tripTable.append(`<tr id="trip-${trip.id}"><td>${trip.name}</td></tr>`);
       });
       $("table, th, td").addClass("table-border");
     })
@@ -23,9 +23,41 @@ const loadTripTable = () => {
      reportStatus(`Encountered an error while loading trips: ${error.message}`);
      console.log(error);
   });
+  $("#trip-table").on("click", "#trip-1", function() {
+    return loadTripDetails();
+});
+};
 
+const loadTripDetails = () => {
+  console.log('Yay you loaded');
+  const tripDetails = $('#trip-details');
+  tripDetails.empty();
+  reportStatus('<strong>Loading trip details...</strong>');
+  tripDetails.append('<table id="trip-card"></table>')
+  const tripCard = $('#trip-card');
+  axios.get(URL + '/1')
+    .then((response) => {
+      reportStatus(`Successfully loaded ${response.data.length} trip`);
+      tripCard.append('<tr><th><h1>Trip Details</h1></th></tr>');
+      const trip = response.data;
+      console.log(trip);
+      tripCard.append(`<tr><td>
+          <h1>Name: ${trip.name}</h1>
+          <p><strong>Continent: <small>${trip.continent}</small></strong></p>
+          <p><strong>Category: <small>${trip.category}</small></strong></p>
+          <p><strong>Weeks: <small>${trip.weeks}</small></strong></p>
+          <p><strong>Cost: <small>$${trip.cost}</small></strong></p>
+          <p><strong>About:</strong></p>
+          <p>${trip.about}</p>
+        </td></tr>`);
+      $("table, th, td").addClass("table-border");
+    })
+    .catch((error) => {
+     reportStatus(`Encountered an error while loading trips: ${error.message}`);
+     console.log(error);
+  });
 };
 
 $(document).ready(() => {
-  $('#load-trips').click(loadTripTable);
+  $('#load-trips').click(loadAllTrips);
 });
